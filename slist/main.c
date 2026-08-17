305d51c9aa9c654be4b0418cd3639058304dc58d
Sharath Kumar <152316074+Sharath0Kumar@users.noreply.github.com>
2026-08-17 15:49:08 +0530

    uploaded the slist folder

---
+++ slist/main.c
@@ -0,0 +1,31 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <assert.h>
+#include "slist.h"
+void test_slist_generic(){
+List* list = slist_new();
+assert(slist_length(list) == 0);
+slist_add_head(list , 10);
+slist_add_head(list , 20);
+assert(slist_length(list) == 2);
+assert(slist_lookup(list , 20));
+slist_add_tail(list , 30);
+slist_add_tail(list , 40);
+slist_add_head(list , 5);
+assert(slist_length(list) == 5);
+slist_delete_head(list);
+assert(slist_length(list) == 4);
+slist_delete_tail(list);
+assert(slist_length(list) == 3);
+slist_free(list);
+assert(slist_length(list) == 0);
+slist_delete_head(list);
+slist_delete_tail(list);
+}
+
+
+int main()
+{
+    test_slist_generic();
+    return 0;
+}
